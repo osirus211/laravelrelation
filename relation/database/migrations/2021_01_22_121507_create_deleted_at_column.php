b5cfb1cbaa68f3c00e1e8bcad105661ff0eba337
sandeep <sandipsolanki@waytoweb.in>
2021-01-22 18:00:43 +0530

    first commit

--- relation/database/migrations/2021_01_22_121507_create_deleted_at_column.php
+++ relation/database/migrations/2021_01_22_121507_create_deleted_at_column.php
@@ -26,7 +26,7 @@ class CreateDeletedAtColumn extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->dropColumn('deleted_at')
+            $table->dropColumn('deleted_at');
         });
     }
 }
